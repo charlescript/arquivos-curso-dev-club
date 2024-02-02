69ca4174322000468600cdd56160cddf759466e9
charlescript <rocha_charles@outlook.com>
2024-02-02 17:37:42 -0300

    biblioteca Math em javascript

---
+++ JAVASCRIPT/math.js
@@ -0,0 +1,11 @@
+/*
+    MATH
+*/
+
+console.log(Math.PI);  // MOSTRAR O VALOR DE PI
+
+console.log( Math.pow(2,3) ); // PERMITE UTILIZAR POTENCIAÇÃO
+console.log( Math.sqrt(9,2) ); // PERMITE UTILIZAR A RAIZ DE UM NÚMERO
+console.log( Math.random() ); // MOSTRA UM NÚMERO ALEATÓRIO
+console.log( Math.ceil(0.5) ); // ARREDONDAR NÚMERO PARA CIMA
+console.log( Math.floor(22.7) ); // ARREDONDAR NÚMERO PARA BAIXO
