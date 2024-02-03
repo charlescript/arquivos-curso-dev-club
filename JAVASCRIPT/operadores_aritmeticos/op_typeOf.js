87b3423b99590135a98502583ca0f828bfcab4e3
charlescript <rocha_charles@outlook.com>
2024-02-03 15:13:32 -0300

    operadores typeof e delete de objects

---
+++ JAVASCRIPT/operadores_aritmeticos/op_typeOf.js
@@ -0,0 +1,20 @@
+/**
+ *  OPERADORES TYPEOF E DELETE
+ */
+
+const myNumber = 20;
+const myString = "Olá mundo!";
+
+
+const myObject = {
+    name: "Object",
+    age: 20,
+    height: 1.70
+}
+
+delete myObject.height;
+
+// console.log(typeof myNumber);
+// console.log(typeof myString);
+console.log(typeof myObject);
+console.log(myObject);
