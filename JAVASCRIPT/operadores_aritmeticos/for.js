640b6c7ad11846358762032cf1e52af2811ae9fb
charlescript <rocha_charles@outlook.com>
2024-02-04 19:25:35 -0300

    for estrutura

---
+++ JAVASCRIPT/operadores_aritmeticos/for.js
@@ -0,0 +1,18 @@
+/**
+ *  ESTRUTURA DE REPETIÇÃO
+ */
+
+
+// let cont = 0;
+
+// for(cont = 1; cont <= 10; cont++){
+//   console.log(cont);
+// }
+
+// console.log("Terminou o for");
+
+const students = ['Bruna', 'Ana', 'João', 'Maria', 'Pedro'];
+
+for(let i = 0; i < students.length; i++){
+    console.log( `${students[i]}  aluno(a) está presente `);
+}
