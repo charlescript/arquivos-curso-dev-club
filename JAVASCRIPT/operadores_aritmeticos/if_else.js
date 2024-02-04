bc6b8a5b1b93b67dac8a5a9a91f5f41254ebd326
charlescript <rocha_charles@outlook.com>
2024-02-03 21:43:51 -0300

    if_else

---
+++ JAVASCRIPT/operadores_aritmeticos/if_else.js
@@ -0,0 +1,14 @@
+// if(true){
+//     console.log('Entrei aqui');
+// }
+
+
+
+const age = 17;
+if(age >= 18){
+    console.log("A pessoa é maior de idade");
+} else {
+    console.log("A pessoa é menor de idade");
+}
+
+console.log('Terminou de executar o código');
