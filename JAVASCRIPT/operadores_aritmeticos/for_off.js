c74d87abeb73b075d76efbc847510b11d74946b6
charlescript <rocha_charles@outlook.com>
2024-02-04 19:46:22 -0300

    FOR OF

---
+++ JAVASCRIPT/operadores_aritmeticos/for_off.js
@@ -0,0 +1,14 @@
+/**
+ *  ESTRUTURA DE REPETIÇÃO = FOR OF
+ */
+
+let myName = 'Charles';
+let allNames = ['Maria', 'João', 'Pedro', 'Gilberto'];
+
+// for (let letter of myName) {
+//     console.log(letter);
+// }
+
+for (let name of allNames) {
+    console.log(name);
+}
