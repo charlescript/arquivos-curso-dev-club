9bf8722edfc9c27e91de29763f241c643ae80289
charlescript <rocha_charles@outlook.com>
2024-02-04 18:26:47 -0300

    switch_case e if_else

---
+++ JAVASCRIPT/operadores_aritmeticos/switch.js
@@ -0,0 +1,19 @@
+let temperature = 36;
+
+switch(temperature){
+    case 30:
+        console.log("A pessoa está com temperatura muito abaixo do normal!");
+    break;
+
+    case 33:
+        console.log("A pessoa está com a temperatura abaixo do normal")
+    break;
+
+    case 36:
+        console.log("A pessoa está com a temperatura normal");
+    break;
+
+    default:
+        console.log('Temperatura não encontrada');
+    break;
+}
