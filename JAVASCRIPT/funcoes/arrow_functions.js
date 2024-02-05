8c685e3be37033a83a690097909df2df5534f89e
charlescript <rocha_charles@outlook.com>
2024-02-05 14:39:08 -0300

    revisão arrow functions

---
+++ JAVASCRIPT/funcoes/arrow_functions.js
@@ -0,0 +1,15 @@
+
+/**
+ *  - Arrow functions
+ */
+
+
+// const sum = function(n1, n2){
+//     const result = n1 + n2;
+//     return result;
+// }
+
+const sum_arrow = (n1, n2) => n1 + n2;
+
+const result = sum_arrow(10,10);  
+console.log(`A soma é = ${result}`);
