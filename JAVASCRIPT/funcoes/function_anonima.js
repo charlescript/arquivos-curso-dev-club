b8d4de8c99601cc4e3c9f1f172a915efd146d0c6
charlescript <rocha_charles@outlook.com>
2024-02-05 14:07:39 -0300

    função anônima

---
+++ JAVASCRIPT/funcoes/function_anonima.js
@@ -0,0 +1,9 @@
+
+const sum = function (n1,n2) { // Dessa forma a função é anônima
+    const result = n1 + n2;
+    return result;
+}
+
+
+const result = sum(10,10);  
+console.log(`A soma é = ${result}`);
