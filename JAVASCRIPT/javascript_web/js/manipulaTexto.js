295ae4a44a046725ad6b28e6a03e3932418df4c7
charlescript <rocha_charles@outlook.com>
2024-02-10 16:47:22 -0300

    Manipulando elementos com javascript

---
+++ JAVASCRIPT/javascript_web/js/manipulaTexto.js
@@ -0,0 +1,5 @@
+const element = document.querySelector('h1');
+
+// element.innerText = "Hello World";
+// element.textContent = "Hello World";
+element.innerHTML = "Hello World <hr/> <h3>My h3 acrescentado</h3>";
