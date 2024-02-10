0a4aab1490859896696cc6dabe843eb9ff4bada7
charlescript <rocha_charles@outlook.com>
2024-02-10 15:40:35 -0300

    Revisando seletores javascript

---
+++ JAVASCRIPT/javascript_web/js/selecionandoElementos.js
@@ -0,0 +1,18 @@
+const element = document.getElementById("main-text");
+
+const paragrafo = document.getElementsByClassName("paragraph");
+
+const paragrafo2 = document.getElementsByTagName("p");
+
+const pegaClassParagrafo = document.querySelector(".paragraph");
+
+const pegaClasseMainText = document.querySelector("#main-text");
+
+const pegaTodosParagrafos = document.querySelectorAll("p");
+
+console.log(element);
+console.log(paragrafo);
+console.log(paragrafo2);
+console.log(pegaClassParagrafo);
+console.log(pegaClasseMainText);
+console.log(pegaTodosParagrafos);
---
+++ JAVASCRIPT/javascript_web/js/selecionandoElementos2.js
@@ -0,0 +1,11 @@
+// const element = document.querySelectorAll('p');
+
+// const myArray = element.forEach(element => {
+//     console.log(element);
+// });
+
+//console.log(myArray);
+
+const element = document.getElementsByName('my-input')
+
+console.log(element);
