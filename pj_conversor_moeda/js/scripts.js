d1a4f5d4c866dd48505d44bdc5a196980642f40a
charlescript <rocha_charles@outlook.com>
2024-02-14 12:50:42 -0300

    Apresentando valores no rodapé de apresentação

--- pj_conversor_moeda/js/scripts.js
+++ pj_conversor_moeda/js/scripts.js
@@ -5,9 +5,23 @@ const dolar = 5.2;
 
 
 const convertValues = () => {
+    // Pegando o volor do real no campo input
     const inputReal = document.getElementById('input-real').value;
+    const realValueText = document.getElementById("real-value-text");
+    
+    // setando valor do real na apresentação
+    realValueText.innerHTML = `R$ ${inputReal}`;
 
-    console.log((inputReal / dolar).toFixed(2));
+    // cnvertendo real em dolar
+    const resultConversao = (inputReal / dolar).toFixed(2);
+    console.log(resultConversao);
+
+
+    // Pegando o valor do dolar e setando na apresentação
+    const dolarValueText = document.getElementById("dolar-value-text");
+    dolarValueText.innerHTML = `US$ ${resultConversao}`; 
+
+    
 }
 
 button.addEventListener('click', convertValues);
