825f1f3cb471544ca35b5c0bda3443bcdcf48e2c
charlescript <rocha_charles@outlook.com>
2024-02-15 14:43:17 -0300

    formatando a moeda corrente com a biblioteca Intl.NumberFormat

--- pj_conversor_moeda/js/scripts.js
+++ pj_conversor_moeda/js/scripts.js
@@ -8,18 +8,26 @@ const convertValues = () => {
     // Pegando o volor do real no campo input
     const inputReal = document.getElementById('input-real').value;
     const realValueText = document.getElementById("real-value-text");
+
+    // Pegando o valor do dolar e setando na apresentação
+    const dolarValueText = document.getElementById("dolar-value-text");
     
     // setando valor do real na apresentação
     realValueText.innerHTML = `R$ ${inputReal}`;
 
+    realValueText.innerHTML = new Intl.NumberFormat('pt-br',{
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputReal);
+    
     // cnvertendo real em dolar
-    const resultConversao = (inputReal / dolar).toFixed(2);
-    console.log(resultConversao);
+    // const resultConversao = (inputReal / dolar).toFixed(2);
+    // dolarValueText.innerHTML = `US$ ${resultConversao}`;
 
-
-    // Pegando o valor do dolar e setando na apresentação
-    const dolarValueText = document.getElementById("dolar-value-text");
-    dolarValueText.innerHTML = `US$ ${resultConversao}`; 
+    dolarValueText.innerHTML = new Intl.NumberFormat('en-US', {
+        style: 'currency', 
+        currency: 'USD'
+    }).format(inputReal / dolar);
 
     
 }
