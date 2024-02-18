171628726290ff5a01d51ac1b7f84c73c4622795
charlescript <rocha_charles@outlook.com>
2024-02-18 01:49:22 -0300

    Usando destructuring assignment

--- projeto_node_teste/first-project-node/index.js
+++ projeto_node_teste/first-project-node/index.js
@@ -3,12 +3,24 @@ const port = 3000;
 
 const app = express();
 
+/**
+ *  - QUery params => meusite.com/users?nome=charles&age=28  // Filtros 
+ *      http://localhost:3000/users?nome=charles&age=28
+ *  - Route params => /users2/    // buscar, deletar ou atualizar algo específico
+ */
+
 app.get('/projects', (request, response) => {
     return response.send('Hello Projects');
 });
 
 app.get('/users', (request, response) => {
-    return response.send('Hello Users');
+    // const name = request.query.name;
+    // const age = request.query.age;
+
+    const { name, age } = request.query;
+
+    
+    return response.json({name: name, age: age});
 });
 
 app.get('/matriculas', (request, response) => {
