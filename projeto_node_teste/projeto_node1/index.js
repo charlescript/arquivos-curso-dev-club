f2ab6fa60a79fe94830ad1f776d6a21694775fc5
charlescript <rocha_charles@outlook.com>
2024-02-21 14:27:49 -0300

    arquivo de debug criado

--- projeto_node_teste/projeto_node1/index.js
+++ projeto_node_teste/projeto_node1/index.js
@@ -27,6 +27,7 @@ const checkUserId = (request, response, next ) => {
     const { id } = request.params;
 
     const index = users.findIndex( user => user.id === id);
+    
     if(index < 0 ){
         return response.status(404).json({ error: "User not found "});
     }
