cb84cf303575094cd012f32f76397029a3c69b80
charlescript <rocha_charles@outlook.com>
2024-02-23 16:41:23 -0300

    Trabalhando com modules

---
+++ JAVASCRIPT/modules/index.js
@@ -0,0 +1,7 @@
+const report = require('./objectsAndFunctions');
+
+console.log(report.sum(2,2));
+
+console.log(report.multi(2,5))
+
+console.log(report.person);
---
+++ JAVASCRIPT/modules/objectsAndFunctions.js
@@ -0,0 +1,15 @@
+function sum (firstNumber , secondNumber){
+    return firstNumber + secondNumber;   
+}
+
+function multi (firstNumber , secondNumber){
+    return firstNumber * secondNumber;   
+}
+
+const person = {
+    name: "Charles",
+    age: 28
+}
+
+module.exports = {sum, multi, person};
+
