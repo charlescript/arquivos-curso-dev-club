87790a15623e797384a573353f2dce5f9e4996db
charlescript <rocha_charles@outlook.com>
2024-02-24 19:15:38 -0300

    verificando escopos

---
+++ JAVASCRIPT/part5/this.js
@@ -0,0 +1,32 @@
+/**
+ *  
+ *  THIS
+ * 
+ *  THIS EM IMGLÊS SIGNIFICA => ESTE, ESTA, ISTO
+ * 
+ *  NO Javascript, this faz referência:
+ * 
+ *  Node => module.exports
+ *  Web => window
+ * 
+ *  Escopo:
+ *      => Global -> Quando começamos a escrever nossa aplicação
+ *          No contexto global, o this faz referência ao objeto global
+ *          que é o objeto window no navegador de internet ou objeto global no Node.js
+ * 
+ *      => Local -> Por exemplo, dentro de uma função
+ */
+
+const name = "Charles";  // Escopo global
+
+function myName() {
+    const myName = "Charles"; // Escopo local
+}
+
+const person = {
+    name: "Charles",
+    age: 25,
+    talk: function() {
+        console.log("Olá");
+    }
+}
