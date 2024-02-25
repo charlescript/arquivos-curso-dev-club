56f7a1fb36a3a0a685c60ba1b2660516c103d367
charlescript <rocha_charles@outlook.com>
2024-02-24 22:40:52 -0300

    Utilizando o this para iportar módulos

--- JAVASCRIPT/part5/modules/objectsAndFunctions.js
+++ JAVASCRIPT/part5/modules/objectsAndFunctions.js
@@ -8,8 +8,22 @@ function multi (firstNumber , secondNumber){
 
 const person = {
     name: "Charles",
-    age: 28
+    age: 28,
+    talk: function(){
+        console.log(this.name);
+    }
 };
+
+//person.talk();
+
+function myFunction(){
+    //console.log(this)
+    console.log("Agora sou o global")
+}
+
+myFunction()
+
+
 
  //module.exports = {sum, multi, person};
 
